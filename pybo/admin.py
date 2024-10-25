8789a8d9ab7c52b1a003ddbb531839c8149e6ca9
penguinjava <aiueoeom97@gmail.com>
2024-10-25 18:22:05 +0900

    2-5번 초반을 작성중..

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question
+from .models import Answer
 
-# Register your models here.
+#class로 데이터를 검색하기
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+# Question을 불러온다.
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
--- pybo/models.py
+++ pybo/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    objects = None
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
 
 
 class Answer(models.Model):
